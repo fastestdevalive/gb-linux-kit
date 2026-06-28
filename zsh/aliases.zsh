f9ebd25bd91289f15f7137792caebf1ea358104f
gb <beebom786@gmail.com>
2026-06-28 01:14:38 -0700

    Fix bklit battery check for zsh glob compatibility

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 # Only define on machines with a battery (laptops)
-if ls /sys/class/power_supply/BAT* &>/dev/null; then
+if ls /sys/class/power_supply/ 2>/dev/null | grep -q '^BAT'; then
   bklit() {
     case "$1" in
       on)  sudo light -s sysfs/leds/tpacpi::kbd_backlight -S 50 ;;
